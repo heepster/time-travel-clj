80716e73b7f64be08bf9138bd67ac5c95dcc89bd
Kevin Huynh <me@khuynh.info>
2016-02-06 19:28:51 -0800

    Getting echo to work

--- src/time_travel_clj/core.clj
+++ src/time_travel_clj/core.clj
@@ -1,12 +1,31 @@
-(ns time-travel-clj.core)
-(require '[clojure.java.shell :only [sh]]
-         '[clojure.string :as str])
+(ns time-travel-clj.core
+  (:require [clojure.java.shell :only [sh]]
+            [clojure.string :as str]))
 
 (def promptStr "time-travel-clj => ")
+
+(defn parse-cmd [cmd]
+  (let [arr (str/split cmd #"\s")]
+  {:cmd (first arr) :args (rest arr)}))
+
+(defn get-cmd [cmd-map]
+  (:cmd cmd-map))
+
+(defn get-args [cmd-map]
+  (:args cmd-map))
+
+(defn cmd-echo [string]
+  (println string))
+
+(defn not-a-cmd [cmd]
+  (println (str " -time-travel-clj: " cmd ": command not found")))
 
 (defn -main [& args]
   (while true
     (print promptStr)
     (flush)
-    (let [input (read-line)]
-      (println "you said " input))))
+    (let [input (read-line)
+          cmd-map (parse-cmd input)]
+      (case (get-cmd cmd-map)
+        "echo" (cmd-echo (get-args cmd-map))
+        (not-a-cmd (get-cmd cmd-map))))))
--- test/time_travel_clj/core_test.clj
+++ test/time_travel_clj/core_test.clj
@@ -1,7 +1,23 @@
 (ns time-travel-clj.core-test
   (:require [clojure.test :refer :all]
-            [time-travel-clj.core :refer :all]))
+            [time-travel-clj.core :as ttc]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parse-cmd-test
+  (testing "parse-cmd"
+    (let [cmd "echo 'hey'"
+          actual-cmd "echo"
+          actual-args "'hey'"
+          output (ttc/parse-cmd cmd)]
+    (is (:cmd output) actual-cmd)
+    (is (:args output) actual-args))))
+
+(deftest get-cmd-test
+  (testing "get-cmd"
+    (let [cmd-map {:cmd "echo" :args "'hello'"}]
+      (is (:cmd cmd-map) (ttc/get-cmd cmd-map)))))
+
+(deftest get-args-test
+  (testing "get-args"
+    (let [cmd-map {:cmd "echo" :args "'hello'"}]
+      (is (:args cmd-map) (ttc/get-args cmd-map)))))
+
