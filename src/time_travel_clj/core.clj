30256d2f3e35bd355825955a54b516253a92c708
Jonathan Li <renderathousand@yahoo.com>
2016-02-16 20:06:16 -0800

    Moved echo to commands and updated core

--- src/time_travel_clj/core.clj
+++ src/time_travel_clj/core.clj
@@ -1,6 +1,8 @@
 (ns time-travel-clj.core
   (:require [clojure.java.shell :only [sh]]
             [clojure.string :as str]
+			[time-travel-clj.stdout :as out]
+			[time-travel-clj.commands.echo :as echo]
             [time-travel-clj.commands.fast-forward-by :as fast-forward-by]
             [time-travel-clj.commands.rewind-by :as rewind-by]
             [time-travel-clj.commands.ls :as ls]
@@ -18,13 +20,6 @@
 (defn get-args [cmd-map]
   (:args cmd-map))
 
-(defn cmd-echo [string]
-  (let [arr (re-seq #"\"[^\"]+\"|\'[^\']+\'|[^\"\']+" string)]
-  (dorun
-  (for [word arr]
-  (if (or (= (first word) \") (= (first word) \')) (print (subs word 1 (- (count word) 1))) (print word))))
-  (println)))
-
 (defn not-a-cmd [cmd]
   (println (str " -time-travel-clj: " cmd ": command not found")))
 
@@ -35,7 +30,7 @@
     (let [input (read-line)
           cmd-map (parse-cmd input)]
       (case (get-cmd cmd-map)
-        "echo" (cmd-echo (str/join " " (get-args cmd-map)))
+        "echo" (echo/execute (get-args cmd-map))
         "ls" (ls/execute (get-args cmd-map))
         "mkdir" (mkdir/execute (get-args cmd-map))
         "rewind-by" (rewind-by/execute (get-args cmd-map))
